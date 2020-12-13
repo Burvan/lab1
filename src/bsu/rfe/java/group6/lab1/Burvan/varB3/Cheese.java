fa4f15fb0e4c883ec00d73a265f5ff0280e48be5
Burvan <burvani@mail.ru>
2020-12-14 00:24:23 +0300

    MetodCode

---
+++ src/bsu/rfe/java/group6/lab1/Burvan/varB3/Cheese.java
@@ -0,0 +1,14 @@
+package bsu.rfe.java.group6.lab1.Burvan.varB3;
+
+public class Cheese extends Food  {
+
+    public Cheese() {
+        super("ׁûנ");
+
+    }
+    public void consume() {
+        System.out.println(this.name + " סתוהום");
+    }
+
+    
+}
